f5229845c91b14a0b9ce77a8c9afb4f321bb7644
danielchristofi <danchristofi@gmail.com>
2025-06-16 16:59:29 +0100

    Added Chromatic aberration

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 A React component that gives a 'Liquid Glass' effect. Currently only works in Chrome and Edge.
 
 Demo: https://liquid-glass-rho.vercel.app/
+
+Guide: https://www.npmjs.com/package/liquid-glass-container
--- src/index.jsx
+++ src/index.jsx
@@ -14,6 +14,7 @@ const LGContainer = ({  contentClass = '',
         warpAmount: 0.07,
         warpDistance: 50,
         warpDirection: -180,
+        caAmount: 2,
     };
 
     const finalConfig = {
@@ -25,6 +26,7 @@ const LGContainer = ({  contentClass = '',
 
     const filter = () => {
         const borderX = Math.min(finalConfig.width, finalConfig.height) * (finalConfig.bevelWidth * 0.5);
+        const rad = finalConfig.radius - (finalConfig.bevelWidth * 50)
 
         const svg = `
 <svg class="displacement-image" viewBox="0 0 ${finalConfig.width} ${finalConfig.height}" xmlns="http://www.w3.org/2000/svg">
@@ -54,7 +56,7 @@ const LGContainer = ({  contentClass = '',
       y="${Math.min(finalConfig.width, finalConfig.height) * (finalConfig.bevelWidth * 0.5)}"
       width="${finalConfig.width - borderX * 2}"
       height="${finalConfig.height - borderX * 2}"
-      rx="${finalConfig.radius}"
+      rx="${rad}"
       fill="hsl(0 0% ${finalConfig.warpDistance}% / ${1 - finalConfig.warpAmount})"
       style="filter:blur(${finalConfig.bevelBlur}px)" />
 </svg>`;
@@ -63,6 +65,29 @@ const LGContainer = ({  contentClass = '',
         const dataUri = `data:image/svg+xml,${encoded}`;
 
         return dataUri;
+    }
+
+    const caMask = () => {
+
+        const borderX = Math.min(finalConfig.width, finalConfig.height) * (finalConfig.bevelWidth * 0.5)
+        const rad = finalConfig.radius - (finalConfig.bevelWidth * 50)
+
+        let svg = `
+<svg viewBox="0 0 ${finalConfig.width} ${finalConfig.height}" xmlns="http://www.w3.org/2000/svg">
+
+    <rect x="${borderX}"
+      y="${Math.min(finalConfig.width, finalConfig.height) * (finalConfig.bevelWidth * 0.5)}"
+      width="${finalConfig.width - borderX * 2}"
+      height="${finalConfig.height - borderX * 2}"
+      rx="${rad}"
+       fill="black" style="filter:blur(${finalConfig.bevelBlur}px)"/>
+
+</svg>`
+
+        const encoded = encodeURIComponent(svg)
+        const dataUri = `data:image/svg+xml,${encoded}`
+
+        return dataUri
     }
 
 
@@ -93,6 +118,8 @@ const LGContainer = ({  contentClass = '',
                 {children}
             </div>
 
+
+            {finalConfig.caAmount === 0 &&
             <svg className="LGFilter" style={filterStyles} xmlns="http://www.w3.org/2000/svg">
                 <defs>
                     <filter id={`${id}-filter`} colorInterpolationFilters="sRGB">
@@ -107,6 +134,56 @@ const LGContainer = ({  contentClass = '',
                     </filter>
                 </defs>
             </svg>
+            }
+
+
+            {finalConfig.caAmount > 0 &&
+                <svg className="LGFilter" style={filterStyles} xmlns="http://www.w3.org/2000/svg">
+                    <defs>
+                        <filter id={`${id}-filter`} colorInterpolationFilters="sRGB">
+                            <feImage x="0" y="0" width="100%" height="100%" result="map" href={filter()}/>
+                            <feDisplacementMap
+                                in2="map"
+                                in="SourceGraphic"
+                                yChannelSelector="B"
+                                xChannelSelector="R"
+                                result="lg"
+                                scale={finalConfig.warpDirection}
+                            />
+
+                            <feColorMatrix in="lg" values="1 0 0 0 0
+														0 0 0 0 0
+														0 0 0 0 0
+														0 0 0 1 0"/>
+                            <feOffset dx={finalConfig.caAmount} result="cr"/>
+                            <feColorMatrix in="lg" values="0 0 0 0 0
+														0 1 0 0 0
+														0 0 0 0 0
+														0 0 0 1 0"/>
+                            <feOffset dy={finalConfig.caAmount} result="cg"/>
+                            <feColorMatrix in="lg" values="0 0 0 0 0
+														0 0 0 0 0
+														0 0 1 0 0
+														0 0 0 1 0" result="cb"/>
+                            <feBlend in="cr" in2="cb" result="b1" mode="screen"/>
+                            <feBlend in="cg" in2="b1" mode="screen" result="ca"/>
+
+                            <feImage x="0" y="0" width="100%" height="100%" result="caMask" href={caMask()}/>
+
+                            <feComponentTransfer in="caMask" result="edge-mask">
+                                <feFuncA type="table" tableValues="1 0"/>
+                            </feComponentTransfer>
+
+                            <feComposite in="ca" in2="edge-mask" operator="in" result="masked-blur"/>
+
+                            <feMerge>
+                                <feMergeNode in="lg"/>
+                                <feMergeNode in="masked-blur"/>
+                            </feMerge>
+                        </filter>
+                    </defs>
+                </svg>
+            }
         </div>
     );
 };
